c7dea58a42794af2f6c229d2e7f2cad135078f8d
deadlock <digospyboss@gmail.com>
2020-03-29 23:40:44 -0300

    included babel, babel polyfill and needed configurations

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import x from './test';
+const y = 25;
 
-console.log(`Number ${x} was imported from another module`);
+console.log(`Number ${x} was imported from another module. Variable x is ${y}.`);
